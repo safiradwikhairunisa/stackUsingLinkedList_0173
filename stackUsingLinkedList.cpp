d38ea21c3ef1a9b756d43d0e252d875f1e766ca5
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-22 10:24:13 +0700

    while 1-3

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -81,4 +81,11 @@ int main()
 
     int choice = 0;
     int value;
+
+    while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+    }
 }
